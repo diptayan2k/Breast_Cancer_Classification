6c2c9299d0f2c8c0c22f43f170b87bb4a841c03b
DIPTAYAN BISWAS <diptayan2k@gmail.com>
2019-11-14 20:37:51 +0530

    Update model.py

--- model.py
+++ model.py
@@ -30,9 +30,9 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting Random Forest Classification to the Training set
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
